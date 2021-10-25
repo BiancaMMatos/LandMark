e7da561103ca30c53b902102dcc664349c3b6301
Bianca Maciel <macielmatosb@gmail.com>
2021-10-25 17:29:25 -0300

    addBronze

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bianca Maciel Matos on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
